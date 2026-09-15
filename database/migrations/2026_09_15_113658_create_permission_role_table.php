b380a8abf0c994f7160ca8a0e92b73f7c1caf575
Mickael Randr <micrandr@gmail.com>
2026-09-15 14:39:07 +0300

    chore(database): prepare users, roles and permissions migrations

---
+++ database/migrations/2026_09_15_113658_create_permission_role_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->uuid('permission_id');
+            $table->uuid('role_id');
+
+            $table->foreign('permission_id')
+                ->references('id')
+                ->on('permissions')
+                ->cascadeOnDelete();
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->cascadeOnDelete();
+
+            $table->timestampTz('created_at');
+
+            $table->primary(['permission_id', 'role_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('permission_role');
+    }
+};
